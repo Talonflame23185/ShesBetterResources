306836d5541602d852799da044194566c55c8d90
Talonflame23185 <connorottbaseball@gmail.com>
2024-12-23 00:08:52 -0700

    Initial Commit

---
+++ ShesBetterResources/ShesBetterResourcesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShesBetterResourcesApp.swift
+//  ShesBetterResources
+//
+//  Created by Connor Ott on 12/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ShesBetterResourcesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShesBetterResourcesTests/ShesBetterResourcesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShesBetterResourcesTests.swift
+//  ShesBetterResourcesTests
+//
+//  Created by Connor Ott on 12/23/24.
+//
+
+import Testing
+@testable import ShesBetterResources
+
+struct ShesBetterResourcesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
